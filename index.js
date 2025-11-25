24ab1b64142ceb5b25ad59e71790f032498cb79e
Isaias Gonzalez <ijgggr@gmail.com>
2025-11-25 15:02:59 -0800

    edit

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ document.getElementById("randomize").addEventListener("click", () => {
   getData().then(members => {
     buf.unshift(randomizeMember(members));
     if (buf.length > 6) { buf.pop(); }
-
     for (let i = 0; i < buf.length; i++) {
       for (let j = 0; j < 4; j++) {
-        const element = document.querySelector(`#m-${i} > #i-${j}`);
+        const element = document.querySelector(`#d-${i} > #p-${j}`);
         element.innerHTML = buf[i][map[j]] ?? "n/a";
       }
     }
