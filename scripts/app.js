b0c603e4b4dfe10d4895bbf5850775a4461e92a9
Isaias Gonzalez <ijgggr@gmail.com>
2025-11-25 14:11:05 -0800

    style

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,9 @@
-//We need to create a app that calls json data internally and displays in on the DOM
-//We need to create a function that will randomize the data being called
-//We want this to happen on some type of event (button click)
-
 let buf = [];
 
-//We need to create a function that will grab the JSON data
-// .then()is a method that waits for a promise to be fufilled
 function getData() {
   return fetch("/data/data.json")
     .then(some => some.json())
     .then(data => {
-      console.log(data);
       return data["members"];
     });
 }
@@ -20,7 +13,6 @@ function randomizeMember(members) {
   return members[randomIndex];
 }
 
-//Create a function that will summon the randomized wizard with a button click
 document.getElementById("randomize").addEventListener("click", () => {
   getData().then(members => {
     let randomMember = randomizeMember(members);
@@ -34,8 +26,8 @@ document.getElementById("randomize").addEventListener("click", () => {
         if (j == 1) { key = "lastName"; }
         if (j == 2) { key = "schoolEmail"; }
         if (j == 3) { key = "personalEmail"; }
-        const temp = document.querySelector(`#m-${i} > #i-${j}`);
-        temp.innerHTML = buf[i][key];
+        const element = document.querySelector(`#m-${i} > #i-${j}`);
+        element.innerHTML = buf[i][key];
       }
     }
   })
