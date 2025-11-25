293dbc28f6bc3d06b233da6fca17eb5d82a06489
Isaias Gonzalez <ijgggr@gmail.com>
2025-11-25 14:40:56 -0800

    js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,19 @@
 let buf = [];
+let map = ["firstName", "lastName", "schoolEmail", "personalEmail"];
+
+document.getElementById("randomize").addEventListener("click", () => {
+  getData().then(members => {
+    buf.unshift(randomizeMember(members));
+    if (buf.length > 6) { buf.pop(); }
+
+    for (let i = 0; i < buf.length; i++) {
+      for (let j = 0; j < 4; j++) {
+        const element = document.querySelector(`#m-${i} > #i-${j}`);
+        element.innerHTML = buf[i][map[j]] ?? "n/a";
+      }
+    }
+  })
+});
 
 function getData() {
   return fetch("/data/data.json")
@@ -12,23 +27,3 @@ function randomizeMember(members) {
   let randomIndex = Math.floor(Math.random() * members.length);
   return members[randomIndex];
 }
-
-document.getElementById("randomize").addEventListener("click", () => {
-  getData().then(members => {
-    let randomMember = randomizeMember(members);
-    buf.unshift(randomMember);
-    if (buf.length > 6) { buf.pop(); }
-
-    for (let i = 0; i < buf.length; i++) {
-      for (let j = 0; j < 4; j++) {
-        let key;
-        if (j == 0) { key = "firstName"; }
-        if (j == 1) { key = "lastName"; }
-        if (j == 2) { key = "schoolEmail"; }
-        if (j == 3) { key = "personalEmail"; }
-        const element = document.querySelector(`#m-${i} > #i-${j}`);
-        element.innerHTML = buf[i][key];
-      }
-    }
-  })
-});
