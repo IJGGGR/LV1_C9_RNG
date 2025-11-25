7ff864b4aa1d62bcf3f6b42d03be3ecaff9e5b63
Isaias Gonzalez <ijgggr@gmail.com>
2025-11-25 11:08:45 -0800

    edit

--- scripts/app.js
+++ scripts/app.js
@@ -2,42 +2,41 @@
 //We need to create a function that will randomize the data being called
 //We want this to happen on some type of event (button click)
 
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
-let house = document.getElementById("house");
-let summonBtn = document.getElementById("summonBtn");
+let buf = [];
 
 //We need to create a function that will grab the JSON data
 // .then()is a method that waits for a promise to be fufilled
-function getData(){
-    return fetch("../data/data.json")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        return data.students;
+function getData() {
+  return fetch("/data/data.json")
+    .then(some => some.json())
+    .then(data => {
+      console.log(data);
+      return data["members"];
     });
 }
 
-getData();
-
-//We need to create a function that will randomize the data being called
-function randomizeData(students){
-    // Math.Random will give the random number but it will be a decimal
-    // Math.floor will round down the decimal to the nearest whole number
-    let randomIndex = Math.floor(Math.random() * students.length);
-    console.log([randomIndex]);
-    return students[randomIndex];
+function randomizeMember(members) {
+  let randomIndex = Math.floor(Math.random() * members.length);
+  return members[randomIndex];
 }
 
 //Create a function that will summon the randomized wizard with a button click
-summonBtn.addEventListener("click",  () => {
-  getData().then((students)=> {
-    let randomStudent = randomizeData(students);
-    console.log(randomStudent);
-    firstName.innerText = randomStudent.firstName;
-    lastName.innerText = randomStudent.lastName;
-    house.innerText = randomStudent.house;
+document.getElementById("randomize").addEventListener("click", () => {
+  getData().then(members => {
+    let randomMember = randomizeMember(members);
+    buf.unshift(randomMember);
+    if (buf.length > 6) { buf.pop(); }
 
+    for (let i = 0; i < buf.length; i++) {
+      for (let j = 0; j < 4; j++) {
+        let key;
+        if (j == 0) { key = "firstName"; }
+        if (j == 1) { key = "lastName"; }
+        if (j == 2) { key = "schoolEmail"; }
+        if (j == 3) { key = "personalEmail"; }
+        const temp = document.querySelector(`#m-${i} > #i-${j}`);
+        temp.innerHTML = buf[i][key];
+      }
+    }
   })
 });
-
